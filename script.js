93f2f2bec6a76281aa6a7365aa66c51d6c0a2624
elaineparas <elainelobete.paras24@bicol-u.edu.ph>
2026-05-04 10:48:59 +0800

    Update 3 files

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-let username = 
-console.log("Username: " + username);
+var form = document.getElementById('loginForm');
+var username = document.getElementById('username');
+var password = document.getElementById('password');
 
-let password = 
-console.log("Password: " + password);
+form.onsubmit = function(e) {
+  e.preventDefault();
+  var u = username.value;
+  var p = password.value;
+  if (p.length < 6 || p.length > 8) {
+    alert('Password must be 6 to 8 characters');
+    return;
+  }
+  alert('Hello ' + u + '! (demo)');
+  console.log('User:', u);
+  form.reset();
+};
